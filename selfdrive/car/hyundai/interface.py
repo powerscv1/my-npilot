2f9035e95b7627c0086da74daceba8d42aed6de2
powerscv1 <79405674+powerscv1@users.noreply.github.com>
2022-10-21 18:00:25 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -25,8 +25,10 @@ class CarInterface(CarInterfaceBase):
 
     v_current_kph = current_speed * CV.MS_TO_KPH
 
-    gas_max_bp = [0., 2., 10., 20., 50., 70., 130., 150.]
-    gas_max_v = [1.15, 1.9,1.7, 1.5, 0.9, 0.47, 0.16, 0.1]
+    #gas_max_bp = [0., 2., 10., 20., 50., 70., 130., 150.]
+    #gas_max_v = [1.15, 1.9,1.7, 1.5, 0.9, 0.47, 0.16, 0.1]
+    gas_max_bp = [0., 5., 20., 50., 70., 130., 150.]
+    gas_max_v = [0.4, 1.7, 1.5, 0.9, 0.47, 0.16, 0.1]
 
     return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
 
@@ -93,17 +95,18 @@ class CarInterface(CarInterfaceBase):
     # longitudinal
     #ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
     #ret.longitudinalTuning.kpV = [1.25, 1.1, 1.0, 0.93, 0.52]
-    ret.longitudinalTuning.kpBP = [0., 2.*CV.KPH_TO_MS, 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.05, 1.13, 1.1, 1.0, 0.93, 0.52]
+    ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 25.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
+    ret.longitudinalTuning.kpV = [0.4, 0.9, 1.15, 0.93, 0.52]
+    #ret.longitudinalTuning.kpV = [1.25, 1.1, 1.0, 0.93, 0.52]
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kiV = [0.1, 0.05]
     ret.longitudinalActuatorDelayLowerBound = 0.5
-    ret.longitudinalActuatorDelayUpperBound = 0.5
+    ret.longitudinalActuatorDelayUpperBound = 0.3
 
     ret.stopAccel = -0.5
     ret.stoppingDecelRate = 0.4  # brake_travel/s while trying to stop
     ret.vEgoStopping = 0.5
-    ret.vEgoStarting = 0.5
+    ret.vEgoStarting = 0.2
 
     # genesis
     if candidate == CAR.GENESIS:
@@ -308,16 +311,16 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.4
       tire_stiffness_factor = 0.8
 
-      ret.steerRatio = 14.5
+      ret.steerRatio = 14.0
       ret.steerRateCost = 0.4
 
       if ret.lateralTuning.which() == 'torque':
         ret.lateralTuning.torque.useSteeringAngle = True
-        max_lat_accel = 2.5
+        max_lat_accel = 3.5
         ret.lateralTuning.torque.kp = 1.0 / max_lat_accel
         ret.lateralTuning.torque.kf = 1.0 / max_lat_accel
         ret.lateralTuning.torque.ki = 0.1 / max_lat_accel
-        ret.lateralTuning.torque.friction = 0.01
+        ret.lateralTuning.torque.friction = 0.05
         ret.lateralTuning.torque.kd = 0.0
 
 
