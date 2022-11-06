5884969ced5567a7858e1ca3ad8d87b95bc19661
powerscv1 <79405674+powerscv1@users.noreply.github.com>
2022-11-06 10:49:17 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -35,7 +35,7 @@ X_EGO_COST = 0.
 V_EGO_COST = 0.
 A_EGO_COST = 0.
 J_EGO_COST = 5.0
-A_CHANGE_COST = 40. # 200.
+A_CHANGE_COST = 45. # 200.
 DANGER_ZONE_COST = 100.
 CRASH_DISTANCE = .5
 LIMIT_COST = 1e6
@@ -43,7 +43,7 @@ ACADOS_SOLVER_TYPE = 'SQP_RTI'
 
 
 CRUISE_GAP_BP = [1., 2., 3., 4.]
-CRUISE_GAP_V = [0.45, 1.1, 1.65, 2.20]
+CRUISE_GAP_V = [0.5, 1.1, 1.65, 2.20]
 
 #AUTO_TR_BP = [0., 30.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
 #AUTO_TR_V = [1.0, 1.2, 1.33, 1.45]
@@ -64,7 +64,7 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
-T_FOLLOW = 1.45
+T_FOLLOW = 1.4
 COMFORT_BRAKE = 2.3 # 2.5
 STOP_DISTANCE = 6.5 # 6.25
 
