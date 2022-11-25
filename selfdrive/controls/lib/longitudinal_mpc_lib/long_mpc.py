b3f54b354c2ceec2f9678f5834b2947c651d5fcc
powerscv1 <79405674+powerscv1@users.noreply.github.com>
2022-11-25 16:31:15 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -35,7 +35,7 @@ X_EGO_COST = 0.
 V_EGO_COST = 0.
 A_EGO_COST = 0.
 J_EGO_COST = 5.0
-A_CHANGE_COST = 50. # 200.
+A_CHANGE_COST = 45. # 200.
 DANGER_ZONE_COST = 100.
 CRASH_DISTANCE = .5
 LIMIT_COST = 1e6
@@ -48,7 +48,7 @@ CRUISE_GAP_V = [0.5, 1.1, 1.65, 2.2]
 #AUTO_TR_BP = [0., 30.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
 #AUTO_TR_V = [1.0, 1.2, 1.33, 1.45]
 AUTO_TR_BP = [0., 15.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 60.*CV.KPH_TO_MS, 80.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
-AUTO_TR_V = [0.6, 0.8, 1.05, 1.2, 1.4, 1.75]
+AUTO_TR_V = [0.65, 0.85, 1.1, 1.25, 1.45, 1.75]
 #AUTO_TR_V=[  0,   15,   20,   60,  80, 110]
 
 AUTO_TR_CRUISE_GAP = 3
@@ -64,9 +64,9 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
-T_FOLLOW = 1.4
-COMFORT_BRAKE = 2.3 # 2.5
-STOP_DISTANCE = 7 # 6.25
+T_FOLLOW = 1.45
+COMFORT_BRAKE = 2.2 # 2.5
+STOP_DISTANCE = 6.5 # 6.25
 
 def get_stopped_equivalence_factor(v_lead):
   return (v_lead**2) / (2 * COMFORT_BRAKE)
