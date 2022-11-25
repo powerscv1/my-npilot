f1b29fcc07e38ba8cb9bb8531283bc4ecd5d1070
powerscv1 <79405674+powerscv1@users.noreply.github.com>
2022-11-25 16:33:44 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -96,17 +96,17 @@ class CarInterface(CarInterfaceBase):
     #ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
     #ret.longitudinalTuning.kpV = [1.25, 1.1, 1.0, 0.93, 0.52]
     ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 25.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [0.35, 0.95, 1.15, 0.93, 0.52]
+    ret.longitudinalTuning.kpV = [0.3, 0.9, 1.15, 0.93, 0.52]
     #ret.longitudinalTuning.kpV = [1.25, 1.1, 1.0, 0.93, 0.52]
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kiV = [0.1, 0.05]
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.3
 
-    ret.stopAccel = -0.5
+    ret.stopAccel = -1.0
     ret.stoppingDecelRate = 0.4  # brake_travel/s while trying to stop
     ret.vEgoStopping = 0.5
-    ret.vEgoStarting = 0.2
+    ret.vEgoStarting = 0.3
 
     # genesis
     if candidate == CAR.GENESIS:
