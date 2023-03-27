92f2cc40f46c8aaa5bb82e8be0e23830ad833c1a
ajouatom <ajouatom@gmail.com>
2023-03-27 13:41:47 +0900

    dh 스티어관련....

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -133,10 +133,10 @@ class CarController:
     # Cut steer actuation bit for two frames and hold torque with induced temporary fault
     max_angle_frames = MAX_ANGLE_FRAMES - 2 if self.CP.carFingerprint in (CAR.HYUNDAI_GENESIS) else MAX_ANGLE_FRAMES
 
-    torque_fault = CC.latActive and self.angle_limit_counter > MAX_ANGLE_FRAMES
+    torque_fault = CC.latActive and self.angle_limit_counter > max_angle_frames
     lat_active = CC.latActive and not torque_fault
 
-    if self.angle_limit_counter >= MAX_ANGLE_FRAMES + MAX_ANGLE_CONSECUTIVE_FRAMES:
+    if self.angle_limit_counter >= max_angle_frames + MAX_ANGLE_CONSECUTIVE_FRAMES:
       self.angle_limit_counter = 0
 
     # CAN-FD platforms
